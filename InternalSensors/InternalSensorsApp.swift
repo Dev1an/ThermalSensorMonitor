6eeea81ea4e71fd262bff685c4733da8a9d2b5fc
Damiaan Dufaux <damiaan.dufaux@theoplayer.com>
2023-09-05 13:29:51 +0200

    Add SwiftUI project

---
+++ InternalSensors/InternalSensorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  InternalSensorsApp.swift
+//  InternalSensors
+//
+//  Created by Damiaan Dufaux on 05/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct InternalSensorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
